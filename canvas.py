454497898a9042ffad4aaacd8ab909ad5d86dec1
FabioTarocco <54395661+FabioTarocco@users.noreply.github.com>
2020-08-31 10:09:32 +0200

    Merge dei due codici

--- canvas.py
+++ canvas.py
@@ -2,6 +2,10 @@ from tkinter import *
 from tkinter import ttk, colorchooser, filedialog
 import PIL
 from PIL import ImageGrab, ImageTk, Image, ImageDraw
+import numpy as np
+import os
+import glob
+
 
 class main:
     def __init__(self,master):
@@ -46,12 +50,21 @@ class main:
             x1= x + self.c.winfo_width()
             y1= y + self.c.winfo_height()
 
-            PIL.ImageGrab.grab().crop((x+2,y+2,x1-2,y1-2)).save(file+'.png')
+            back_to_garyscale(PIL.ImageGrab.grab().crop((x+2,y+2,x1-2,y1-2))).save(file+'.png')
+          
+
+            files = glob.glob('pictures/temp/*')
+            for f in files:
+                os.remove(f)
+
+            
 
     def importImage(self):
         file=filedialog.askopenfilename()
         self.file=file
-        self.image2 = ImageTk.PhotoImage(file = self.file)
+        rgb_image=colorize_image(Image.open(self.file))
+        rgb_image.save('pictures/temp/'+self.file[len(self.file)-12:])
+        self.image2 = ImageTk.PhotoImage(file = 'pictures/temp/'+self.file[len(self.file)-12:])
         print(type(self.image2))
         self.c.create_image(96,130,image = self.image2, anchor=CENTER)
     def clear(self):
@@ -117,6 +130,76 @@ class main:
         menu.add_cascade(label='Option..', menu=optionmenu)
         optionmenu.add_command(label = 'Clear Canvas', command = self.clear)
         optionmenu.add_command(label = 'Exit', command = self.master.destroy)
+
+
+def colorize_image(image):
+	# Converte l'immagine da GRAYSCALE a RGB mantenendo la size
+	rgb_image = Image.new('RGB', image.size)
+	rgb_image.paste(image)
+
+	w, h = rgb_image.size
+
+	# Itera sui pixel dell'immagine per cambio colore
+	for x in range(0, w - 1):
+		for y in range(0, h - 1):
+			# Acquisisce il valore RGB del pixel
+			r, g, b = rgb_image.getpixel((x, y))
+
+			# Definizione dei colori
+			# Red
+			new_color_right_arm = 235, 64, 52
+			# Yellow
+			new_color_pants = 252, 186, 3
+			# Green
+			new_color_left_arm = 50, 168, 82
+			# Blue
+			new_color_shirt = 66, 135, 245
+			# Purple
+			new_color_face = 128, 52, 235
+			# Cyan
+			new_color_hair = 52, 235, 217
+
+			# Cambia colori
+			if r == g == b == 13:
+				rgb_image.putpixel((x, y), new_color_right_arm)
+			elif r == g == b == 8:
+				rgb_image.putpixel((x, y), new_color_pants)
+			elif r == g == b == 11:
+				rgb_image.putpixel((x, y), new_color_left_arm)
+			elif r == g == b == 4:
+				rgb_image.putpixel((x, y), new_color_shirt)
+			elif r == g == b == 12:
+				rgb_image.putpixel((x, y), new_color_face)
+			elif r == g == b == 1:
+				rgb_image.putpixel((x, y), new_color_hair)
+
+	return rgb_image
+
+
+def back_to_garyscale(image):
+	gs_image = image.convert('L')
+
+	h, w = gs_image.size
+
+	for x in range(0, h - 1):
+		for y in range(0 , w -1):
+			gs = gs_image.getpixel((x, y))
+
+			if gs == 114:
+				gs_image.putpixel((x, y), 13)
+			elif gs == 185:
+				gs_image.putpixel((x, y), 8)
+			elif gs == 123:
+				gs_image.putpixel((x, y), 11)
+			elif gs == 127:
+				gs_image.putpixel((x, y), 4)
+			elif gs == 96:
+				gs_image.putpixel((x, y), 12)
+			elif gs == 178:
+				gs_image.putpixel((x, y), 1)
+
+	return gs_image
+
 
 if __name__=='__main__':
     root = Tk()
