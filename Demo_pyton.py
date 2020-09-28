dc6f2c5ef0193f7917f57af53a53ec2e8f5da076
FabioTarocco <54395661+FabioTarocco@users.noreply.github.com>
2020-09-28 10:09:40 +0200

    modificate cartelle

--- Demo_pyton.py
+++ Demo_pyton.py
@@ -23,23 +23,23 @@ root = Tk()
 root.title("PYTON")
 
 
-img1 = ImageTk.PhotoImage(Image.open('./pictures/000812_0.jpg'))
+img1 = ImageTk.PhotoImage(Image.open('./pictures/models/000812_0.jpg'))
 img_1= Label(root, image=img1)
 img_1.grid(row=0, column=0)
 
-img2 = ImageTk.PhotoImage(Image.open('./pictures/000988_0.jpg'))
+img2 = ImageTk.PhotoImage(Image.open('./pictures/models/000988_0.jpg'))
 img_2= Label(root, image=img2)
 img_2.grid(row=0, column=1)
 
-img3 = ImageTk.PhotoImage(Image.open('./pictures/000001_0.jpg'))
+img3 = ImageTk.PhotoImage(Image.open('./pictures/models/000001_0.jpg'))
 img_3= Label(root, image=img3)
 img_3.grid(row=0, column=2)
 
-img4 = ImageTk.PhotoImage(Image.open('./pictures/000174_0.jpg'))
+img4 = ImageTk.PhotoImage(Image.open('./pictures/models/000174_0.jpg'))
 img_4= Label(root, image=img4)
 img_4.grid(row=0, column=3)
 
-img5 = ImageTk.PhotoImage(Image.open('./pictures/002474_0.jpg'))
+img5 = ImageTk.PhotoImage(Image.open('./pictures/models/002474_0.jpg'))
 img_5= Label(root, image=img5)
 img_5.grid(row=0, column=4)
 
@@ -56,23 +56,23 @@ img_lbl_3.grid(row=1, column=2)
 img_lbl_4.grid(row=1, column=3)
 img_lbl_5.grid(row=1, column=4)
 
-dress1 = ImageTk.PhotoImage(Image.open('./pictures/000192_1.jpg'))
+dress1 = ImageTk.PhotoImage(Image.open('./pictures/dresses/000192_1.jpg'))
 dress_1= Label(root, image=dress1)
 dress_1.grid(row=2, column=0)
 
-dress2 = ImageTk.PhotoImage(Image.open('./pictures/000445_1.jpg'))
+dress2 = ImageTk.PhotoImage(Image.open('./pictures/dresses/000445_1.jpg'))
 dress_2= Label(root, image=dress2)
 dress_2.grid(row=2, column=1)
 
-dress3 = ImageTk.PhotoImage(Image.open('./pictures/001301_1.jpg'))
+dress3 = ImageTk.PhotoImage(Image.open('./pictures/dresses/001301_1.jpg'))
 dress_3= Label(root, image=dress3)
 dress_3.grid(row=2, column=2)
 
-dress4 = ImageTk.PhotoImage(Image.open('./pictures/001879_1.jpg'))
+dress4 = ImageTk.PhotoImage(Image.open('./pictures/dresses/001879_1.jpg'))
 dress_4= Label(root, image=dress4)
 dress_4.grid(row=2, column=3)
 
-dress5 = ImageTk.PhotoImage(Image.open('./pictures/002337_1.jpg'))
+dress5 = ImageTk.PhotoImage(Image.open('./pictures/dresses/002337_1.jpg'))
 dress_5= Label(root, image=dress5)
 dress_5.grid(row=2, column=4)
 
